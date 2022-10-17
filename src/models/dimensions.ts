d749db6e08a27b0fc45a6f19d0ab57075d8ae383
Artem Chepelevich <artem.chepelevich@gismart.com>
2022-10-17 20:54:39 +0400

    chore: add necessary styles prop models

---
+++ src/models/dimensions.ts
@@ -0,0 +1,42 @@
+export interface IDimensionsProps {
+  /**
+   * Define minimum width in px
+   */
+  minWidth?: number
+  /**
+   * Define maximum width in px
+   */
+  maxWidth?: number
+  /**
+   * Define element width in px
+   */
+  width?: number
+  /**
+   * Define minimum height in px
+   */
+  minHeight?: number
+  /**
+   * Define maximum height in px
+   */
+  maxHeight?: number
+  /**
+   * Define element height in px
+   */
+  height?: number
+  /**
+   * Set element padding
+   */
+  padding?: string
+  /**
+   * Set element margin
+   */
+  margin?: string
+  /**
+   * Should element fill all available width
+   */
+  isFullWidth?: boolean
+  /**
+   * Define element border radius
+   */
+  borderRadius?: string
+}
---
+++ src/models/fonts.ts
@@ -0,0 +1,14 @@
+export interface IFontsProps {
+  /**
+   * Text size in px
+   */
+  fontSize?: number
+  /**
+   * Line height in px
+   */
+  lineHeight?: number
+  /**
+   * Text weight
+   */
+  fontWeight?: number
+}
