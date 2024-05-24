0fc4060a509183d0f7c98dc806fb98a22903f5b4
Artem Chepelevich <artem.chepelevich@gismart.com>
2024-05-24 14:20:05 +0200

    chore: export components only

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import * as components from './components'
-import * as constants from './constants'
-import {
-  CommonColorsStyles,
-  CommonDimensionsStyles,
-  CommonFontsStyles,
-  CommonAnswerStyles,
-} from './styles'
-
-export {
-  components,
-  constants,
-  CommonColorsStyles,
-  CommonDimensionsStyles,
-  CommonFontsStyles,
-  CommonAnswerStyles,
-}
+export * from './components'
